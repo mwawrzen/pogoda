5be45442899785da7627493a3c09eb10d2a4d3e4
mwawrzen <w.w@op.pl>
2021-05-26 04:31:47 +0200

    Create function getting date from api

--- pogoda/Services/DateService.cs
+++ pogoda/Services/DateService.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using pogoda.Models;
 
 namespace pogoda.Services
 {
     class DateService
     {
+        const string API = "http://worldtimeapi.org/api/timezone/Europe/Warsaw";
 
+        public static async Task<DateTime> GetDate()
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var responseMessage = await client.GetAsync(API);
+            var resultArray = await responseMessage.Content.ReadAsStringAsync();
+            var date = JsonConvert.DeserializeObject<Date>(resultArray);
+
+            string dateString = date.datetime;
+            DateTime currDate = Convert.ToDateTime(dateString);
+
+            return currDate;
+        }
     }
 }

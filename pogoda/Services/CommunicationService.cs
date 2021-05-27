02ff8275a0dae4382c06206df4744e6d3ca50284
mwawrzen <w.w@op.pl>
2021-05-27 16:29:59 +0200

    Create communication with arduino service

---
+++ pogoda/Services/CommunicationService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace pogoda.Services
+{
+    public class CommunicationService
+    {
+
+        private static String[]? Ports;
+        private static SerialPort? Port;
+
+        public static void Connect(string message)
+        {
+            Ports = SerialPort.GetPortNames();
+
+            if (Ports.Length != 0)
+            {
+                foreach (var port in Ports)
+                {
+                    Console.WriteLine(port);
+                }
+
+                Port = new SerialPort(Ports[0], 9600, Parity.None, 8, StopBits.One);
+                Port.Open();
+                Port.Write(message);
+            }
+
+            else
+            {
+                Console.WriteLine("Nie znaleziono zadnych portow");
+            }
+        }
+    }
+}
+

609feb119e327415b8a836184a2dd163628e218a
mwawrzen <w.w@op.pl>
2021-05-27 09:11:27 +0200

    Create measurement model

---
+++ pogoda/Models/Measurement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace pogoda.Models
+{
+    public class Measurement
+    {
+        public string? day { get; set; }
+        public double value { get; set; }
+    }
+
+    public class StationMeasurement
+    {
+        public string? station { get; set; }
+        public List<Measurement> temperature { get; set; }
+        public List<Measurement> pressure { get; set; }
+        public List<Measurement> moisture { get; set; }
+        public List<Measurement> windSpeed { get; set; }
+    }
+}
